e2f942386a013bffbb696a8f1dba4d4bcf02a301
nkshe <116078049+nkshe@users.noreply.github.com>
2022-10-18 05:29:48 -0700

    Add files via upload

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
+AUUID="36c9cb20-c239-4a70-8273-5da229049f62"
+CADDYIndexPage="https://raw.githubusercontent.com/caddyserver/dist/master/welcome/index.html"
+CONFIGCADDY="https://raw.githubusercontent.com/jssame/cxdxy/main/etc/Caddyfile"
+CONFIGXRAY="https://raw.githubusercontent.com/jssame/cxdxy/main/etc/xray.json"
+ParameterSSENCYPT="chacha20-ietf-poly1305"
+
+# configs
+mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
+wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
+wget -qO- $CONFIGCADDY | sed -e "1c :$PORT" -e "s/\$AUUID/$AUUID/g" -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $AUUID)/g" >/etc/caddy/Caddyfile
+wget -qO- $CONFIGXRAY | sed -e "s/\$AUUID/$AUUID/g" -e "s/\$ParameterSSENCYPT/$ParameterSSENCYPT/g" >/xray.json
+
+# start
 tor &
 
 /xray -config /xray.json &
